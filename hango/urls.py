7151d038d7f76bad618426f364d55daa30448a44
Julles Pio <jullespio@tutanota.com>
2025-08-26 12:28:35 -0300

    Menu style updated; new About page links

--- hango/urls.py
+++ hango/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView  # ← add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.menu.urls')),
     path('orders/', include('apps.orders.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # ← adds login/logout/password reset
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),  # ← new
 ]
