a1f3e6d0cd5ca97ee405b86b56b9371e429f36d9
Julles Pio <jullespio@tutanota.com>
2025-10-06 15:26:25 -0300

    Ajustes no texto da página

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -5,4 +5,4 @@ class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.accounts"
     label = "accounts"
-    verbose_name = _("Accounts")  # <-- translatable app name
+    verbose_name = "Contas"
